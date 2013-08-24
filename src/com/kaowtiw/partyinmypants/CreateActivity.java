1bbdb4b4a828485abe529bd46ee5676889486687
JoelV <joel.villahermosa@gmail.com>
2013-08-23 23:12:27 -0400

    got the two activities added to manifest

--- src/com/kaowtiw/partyinmypants/CreateActivity.java
+++ src/com/kaowtiw/partyinmypants/CreateActivity.java
@@ -2,11 +2,14 @@ package com.kaowtiw.partyinmypants;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CreateActivity extends Activity {
+	TextView tv;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//tv = (TextView) findViewById(R.id.title);
+		//tv.setText("Hello World");
 	}
 
 }
