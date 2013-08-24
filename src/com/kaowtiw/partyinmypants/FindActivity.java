642c21d96b6ed20ae711d62eb26f7ca28a37cc4f
JoelV <joel.villahermosa@gmail.com>
2013-08-24 02:49:14 -0400

    Login works :D

--- src/com/kaowtiw/partyinmypants/FindActivity.java
+++ src/com/kaowtiw/partyinmypants/FindActivity.java
@@ -1,4 +1,4 @@
-Ovpackage com.kaowtiw.partyinmypants;
+package com.kaowtiw.partyinmypants;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,6 +7,8 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,6 +33,16 @@ public class FindActivity extends Activity {
 			list.add(item);
 		}
 		
+		StableArrayAdapter adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 	
 	private class StableArrayAdapter extends ArrayAdapter<String>{
@@ -42,7 +54,16 @@ public class FindActivity extends Activity {
 				idMap.put(objects.get(i), i);
 			}
 		}
-	}
 	
-	@
+		@Override
+		public long getItemId(int position) {
+			String item = getItem(position);
+			return idMap.get(item);
+		}
+		
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+	}
 }
--- src/com/kaowtiw/partyinmypants/LoginActivity.java
+++ src/com/kaowtiw/partyinmypants/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.kaowtiw.partyinmypants;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 	EditText userName, password;
 	Button submit, newUser;
+	private ProgressDialog pDialog;
+	JSONParser jsonParser = new JSONParser();
+	private static final String LOGIN_URL = "http://www.joelv.net/partyapp/login.php";
+	private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +45,7 @@ public class LoginActivity extends Activity {
 	
 	OnClickListener submitClick = new OnClickListener() {
 		public void onClick(View v) {
-			startActivity(new Intent(LoginActivity.this, MainActivity.class));
+			new AttemptLogin().execute();
 		}
 	};
 	
@@ -37,4 +54,75 @@ public class LoginActivity extends Activity {
 			startActivity(new Intent(LoginActivity.this, CreateUserActivity.class));
 		}
 	};
+	class AttemptLogin extends AsyncTask<String, String, String> {
+
+		 /**
+        * Before starting background thread Show Progress Dialog
+        * */
+		boolean failure = false;
+
+       @Override
+       protected void onPreExecute() {
+           super.onPreExecute();
+           pDialog = new ProgressDialog(LoginActivity.this);
+           pDialog.setMessage("Attempting login...");
+           pDialog.setIndeterminate(false);
+           pDialog.setCancelable(true);
+           pDialog.show();
+       }
+
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			 // Check for success tag
+           int success;
+           String username = userName.getText().toString();
+           String passwordStr = password.getText().toString();
+           try {
+               // Building Parameters
+               List<NameValuePair> params = new ArrayList<NameValuePair>();
+               params.add(new BasicNameValuePair("username", username));
+               params.add(new BasicNameValuePair("password", passwordStr));
+
+               Log.d("request!", "starting");
+               // getting product details by making HTTP request
+               JSONObject json = jsonParser.makeHttpRequest(
+                      LOGIN_URL, "POST", params);
+
+               // check your log for json response
+               Log.d("Login attempt", json.toString());
+
+               // json success tag
+               success = json.getInt(TAG_SUCCESS);
+               if (success == 1) {
+               	Log.d("Login Successful!", json.toString());
+               	Intent i = new Intent(LoginActivity.this, MainActivity.class);
+               	finish();
+   				startActivity(i);
+               	return json.getString(TAG_MESSAGE);
+               }else{
+               	Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+               	return json.getString(TAG_MESSAGE);
+
+               }
+           } catch (JSONException e) {
+               e.printStackTrace();
+           }
+
+           return null;
+
+		}
+		/**
+        * After completing background task Dismiss the progress dialog
+        * **/
+       protected void onPostExecute(String file_url) {
+           // dismiss the dialog once product deleted
+           pDialog.dismiss();
+           if (file_url != null){
+           	Toast.makeText(LoginActivity.this, file_url, Toast.LENGTH_LONG).show();
+           }
+
+       }
+
+	}
 }
